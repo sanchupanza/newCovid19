8da4db24d912928b6b16db27ee7f9c0527d9a48c
Siddhesh <siddhesh7987@gmail.com>
2020-04-30 01:47:36 +0530

    Resoureces Detail Activity created Designed Done And some ui changes

---
+++ app/src/main/java/com/sanchit/covid19tracker/Activities/ResourcesDetailActivity.java
@@ -0,0 +1,19 @@
+package com.sanchit.covid19tracker.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sanchit.covid19tracker.R;
+
+public class ResourcesDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resources_detail);
+
+
+
+    }
+}
